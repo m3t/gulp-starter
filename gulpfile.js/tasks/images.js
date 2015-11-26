ffbaef6dbb69b4ec5044f109e73d31a984cc4fc7
m3t <mlt@posteo.de>
2015-11-27 00:20:20 +0100

    Fix images.js

--- gulpfile.js/tasks/images.js
+++ gulpfile.js/tasks/images.js
@@ -8,14 +8,14 @@ var imagemin    = require('gulp-imagemin')
 var path        = require('path')
 
 var paths = {
-  src: path.join(config.root.src, config.tasks.images.src, '/**'),
+  src: path.join(config.root.src, config.tasks.images.src, '/**/*.{jpg,png,gif}'),
   dest: path.join(config.root.dest, config.tasks.images.dest)
 }
 
 var imagesTask = function() {
   return gulp.src(paths.src)
-    .pipe(changed(paths.dest)) // Ignore unchanged files
-    .pipe(imagemin()) // Optimize
+//    .pipe(changed(paths.dest)) // Ignore unchanged files
+    .pipe(imagemin({ progressive: true })) // Optimize
     .pipe(gulp.dest(paths.dest))
 //    .pipe(browserSync.stream())
 }
